cb77f03a113212a3b8148a1c6d5b4424ea00124c
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2025-07-06 22:15:04 +0500

    fix code

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const apiKey = 'ec2a47e3143f8078b58168d360841d61'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q='
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const searchBox = document.querySelector('.search input')
 const searchBtn = document.querySelector('.search button')
 const error = document.querySelector('.error')
+const weather = document.querySelector('.weather') // Assuming you have a weather container
 
-async function checkWeather(city){
-    const response =  await fetch(apiUrl + city + `&appid=${apiKey}`)
-    var data = await response.json()
-    console.log(data)   
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
 
-    document.querySelector('.city').innerHTML = data.name
-    document.querySelector('.temp').innerHTML =  Math.round(data.main.temp)+ '°c'
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '  %';
-    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+    if (response.status === 404) {
+       
+        error.style.display = 'block'
+        weather.style.display = 'none'
+    } else {
+        let data = await response.json()
+
+        document.querySelector('.city').innerHTML = data.name
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C'
+        document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
+        document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+
+        weather.style.display = 'block'
+        error.style.display = 'none'
+    }
 }
-searchBtn.addEventListener('click',()=>{
+
+searchBtn.addEventListener('click', () => {
     checkWeather(searchBox.value)
-   
 })
